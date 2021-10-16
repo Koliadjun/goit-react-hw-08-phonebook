f18ca261a371aac62ca141f911d714dfaece5a2b
Koliadjun <koliadjun@gmail.com>
2021-10-16 21:30:34 +0300

    added autp selectors

---
+++ src/redux/authorization/authorization-selectors.js
@@ -0,0 +1,5 @@
+export const getUserName = state => state.auth.user.name;
+
+export const getIsLoggedIn = state => state.auth.isLoggedIn;
+
+export const getIsFetchingCurrent = state => state.auth.isFetchingCurrUser;
