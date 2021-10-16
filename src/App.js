0d1b1e0914004cf2f33c6a1ae3d63cb796d1ce77
Koliadjun <koliadjun@gmail.com>
2021-10-17 02:21:25 +0300

    route, user is logined

--- src/App.js
+++ src/App.js
@@ -1,32 +1,48 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import NavBar from 'components/NavBar/NavBar';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import MainView from 'view/MainView/MainView';
 import ContactsView from 'view/ContactsView/ContactsView';
 import LoginView from 'view/LoginView/LoginView';
 import RegisterView from 'view/RegisterVIew/RegisterView';
+import { useDispatch, useSelector } from 'react-redux';
+import { authOperations, authSelectors } from 'redux/authorization';
+import PublicRoute from 'components/PublicRoute';
+import PrivateRoute from 'components/PrivateRoute';
+import Container from 'components/Container/Container';
 
 const App = () => {
+  const dispatch = useDispatch();
+  const isFetchingCurrentUser = useSelector(authSelectors.getIsFetchingCurrent);
+
+  useEffect(() => {
+    dispatch(authOperations.refreshUser());
+  }, [dispatch]);
   return (
-    <>
-      <NavBar />
-      <Switch>
-        <Route path='/' exact >
-          <MainView />
-        </Route>
-        <Route path='/contacts' exact >
-          <ContactsView />
-        </Route>
-        <Route path='/login' exact >
-          <LoginView />
-        </Route>
-        <Route path='/register' exact >
-          <RegisterView />
-        </Route>
-      </Switch>
-    </>
-  );
+    <Container>
+      {isFetchingCurrentUser ? (
+        <h1> Завантаження</h1 >
+      ) : (
+        <>
+          <NavBar />
+          <Switch>
+            <PublicRoute exact path="/">
+              <MainView />
+            </PublicRoute>
+            <PrivateRoute path="/contacts" redirectTo="/login">
+              <ContactsView />
+            </PrivateRoute>
+            <PublicRoute exact path="/login" redirectTo="/contacts" restricted>
+              <LoginView />
+            </PublicRoute>
+            <PublicRoute exact path="/register" restricted>
+              <RegisterView />
+            </PublicRoute>
+          </Switch>
+        </>)
+      }
+    </Container>);
 }
 export default App;
