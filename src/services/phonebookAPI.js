be602cf5fefd3fd77f086ecb80a24469f518b391
Koliadjun <koliadjun@gmail.com>
2021-10-16 20:27:15 +0300

    UpdateAPI

--- src/services/phonebookAPI.js
+++ src/services/phonebookAPI.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// axios.defaults.baseURL = 'http://localhost:4000';
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
-axios.defaults.baseURL = 'https://contacts-json-api.herokuapp.com/';
+// axios.defaults.baseURL = 'https://contacts-json-api.herokuapp.com';
 
 export async function fetchContacts() {
   const { data } = await axios.get(`/contacts`);
@@ -15,4 +15,20 @@ export async function addContact(contact) {
 }
 export async function deleteContactById(contactId) {
   await axios.delete(`/contacts/${contactId}`);
+}
+
+export async function registerUser(formData) {
+  await axios.post(`/users/signup`, formData);
+}
+
+export async function logInUser(formData) {
+  await axios.post(`/users/login`, formData);
+}
+
+export async function logOutUser() {
+  await axios.post(`/users/logout`);
+}
+
+export async function refreshCurrUser() {
+  await axios.get(`/users/logout`);
 }
