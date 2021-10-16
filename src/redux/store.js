01c6a13b5fab7b2325fe16a3b5c64802b76b15a1
Koliadjun <koliadjun@gmail.com>
2021-10-16 21:30:00 +0300

    add auth store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import contactReducer from './Phonebook/phonebook-reducer';
+import authReducer from './authorization/authorization-slice'
 const store = configureStore({
   reducer: {
     contacts: contactReducer,
+    auth: authReducer
   },
   devTools: process.env.NODE_ENV !== 'production',
 });
