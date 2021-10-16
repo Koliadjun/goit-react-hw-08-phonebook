59c53f13d32920a75bdf0674167fd3fc89371d53
Koliadjun <koliadjun@gmail.com>
2021-10-16 18:50:17 +0300

    create views

---
+++ src/view/ContactsView/ContactsView.jsx
@@ -0,0 +1,10 @@
+import Phonebook from 'components/Phonebook';
+import React from 'react';
+
+export default function ContactsView() {
+  return (
+    <section>
+      <Phonebook />
+    </section>
+  );
+}
---
+++ src/view/MainView/MainView.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Main() {
+  return (
+    <section>
+      <h1>Здоровенькі були! Ласкаво просимо Вас на наш сервіс!</h1>
+    </section>
+  );
+}
