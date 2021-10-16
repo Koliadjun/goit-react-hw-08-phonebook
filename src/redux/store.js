b127f30f4eb9c7d29ee33f9b6467118b2307e1e8
Koliadjun <koliadjun@gmail.com>
2021-10-17 02:22:56 +0300

    added persistor

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,38 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import contactReducer from './Phonebook/phonebook-reducer';
-import authReducer from './authorization/authorization-slice'
-const store = configureStore({
+import authReducer from 'redux/authorization/authorization-slice'
+
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+];
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
+export const store = configureStore({
   reducer: {
     contacts: contactReducer,
-    auth: authReducer
+    auth: persistReducer(authPersistConfig, authReducer),
   },
+  middleware,
   devTools: process.env.NODE_ENV !== 'production',
 });
 
-export default store;
+export const persistor = persistStore(store);
