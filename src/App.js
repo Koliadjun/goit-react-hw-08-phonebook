92ba45361b8c528dda7287d2bc513e9d3e8f350a
Koliadjun <koliadjun@gmail.com>
2021-10-16 18:50:31 +0300

    Link view to route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import './App.css';
 import React from 'react';
 
-import Phonebook from './components/Phonebook';
-import data from './components/data.json';
+import NavBar from 'components/NavigationLink/NavigationLink';
+import { Switch, Route } from 'react-router-dom';
+import MainView from 'view/MainView/MainView';
+import ContactsView from 'view/ContactsView/ContactsView';
+import LoginView from 'view/LoginView/LoginView';
+import RegisterView from 'view/RegisterVIew/RegisterView';
 
 const App = () => {
-  return <Phonebook contacts={data} />;
-};
-
+  return (
+    <>
+      <NavBar />
+      <Switch>
+        <Route path='/' exact >
+          <MainView />
+        </Route>
+        <Route path='/contacts' exact >
+          <ContactsView />
+        </Route>
+        <Route path='/login' exact >
+          <LoginView />
+        </Route>
+        <Route path='/register' exact >
+          <RegisterView />
+        </Route>
+      </Switch>
+    </>
+  );
+}
 export default App;
